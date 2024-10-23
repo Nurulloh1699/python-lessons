3fadf5eeea6ea4b634e7bb0093f222111c97c729
Nurulloh Abdurashidov <nur206677@gmail.com>
2024-10-23 11:17:03 +0500

    Add files via upload

--- 11-dars_Dictionary_(Lug'at).py
+++ 11-dars_Dictionary_(Lug'at).py
@@ -5,7 +5,7 @@ Created on Wed Oct 23 08:41:28 2024
 @author: DavrServis
 """
 
-#10-dars Dictionary (Lug'at).
+#11-dars Dictionary (Lug'at).
 #23.10.2024.
 #Muallif:Abdurashidov Nurulloh.
 
@@ -54,21 +54,68 @@ Created on Wed Oct 23 08:41:28 2024
 # print(talaba_1)
 
 # Lug'atlarni bir nechta qatorga yozishimiz ham mumkin buning uchun quyidagicha yo'l tutamiz.
-telefonlar = {
-    'ali':'iphone x',
-    'vali':'nokia',
-    'shodi':'samsung',
-    'qodir':'motorolla'
-    } # shu ko'rinishda.
+# telefonlar = {
+#     'ali':'iphone x',
+#     'vali':'nokia',
+#     'shodi':'samsung',
+#     'qodir':'motorolla'
+#     } # shu ko'rinishda.
 
 # Lug'at ichida element bor yo'qligiga qarab chiqadigan habarni tahlil qilamiz.
 # Lug'atda yoq element kiritilganda xatolik chiqishini oldini olish uchun biz (.get) metodidan
 # foydalanamiz.
-phone = telefonlar.get('hasan','Bunday ism mavjud emas!') # Bu yerda biz (.get) metodidan keyin
+# phone = telefonlar.get('hasan','Bunday ism mavjud emas!') # Bu yerda biz (.get) metodidan keyin
 # ikkita qiymat bedik.
-print(phone)
+# print(phone)
 
 # Agar yuqoridagidek ikkita qiymat berilmasa ham qo'rqinchili emas, quyida buni ko'rib chiqamiz.
-phone = telefonlar.get('hasan')
-print(phone) # Bu yerda (none) javob qaytaradi, va bu ham qaysidur manoda foydalanuvchiga 
+# phone = telefonlar.get('hasan')
+# print(phone) # Bu yerda (none) javob qaytaradi, va bu ham qaysidur manoda foydalanuvchiga 
 # tushunarli bo'ladi.
+
+# TOPSHIRIQLAR.
+#1 otam (onam, akam, ukam, va hokazo) degan lug'at yarating va lug'atga shu inson haqida
+# kamida 3 ta m'alumot kiriting (ismi, tu'gilgan yili, shahri, manzili va hokazo). Lug'atdagi
+# ma'lumotni matn shaklida konsolga chiqaring :Otamning ismi Mavlutdin, 1954-yilda, Samarqand
+# viloyatida tug'ilgan.
+# otam = {'ism_sh':'Maksimov Yorqinjon', 'yosh':'54','t_yil':'1970','t_joy':'Oltiariq tumani'}
+# print(f"Otamning ism shariflari {otam['ism_sh']}, ular {otam['t_yil']}-yilda tug'ilganlar,\
+# yoshlari {otam['yosh']}da, tug'ilgan joylari {otam['t_joy']}.")
+
+#2 Oila a'zolaringizning sevimli taomlari lug'atini tuzing. Lug'atda kamida 5 ta ism-taom
+# jufltigi bo'lsin. Kamida uch kishining sevimli taomini konsolga chiqaring:
+#Alining sevimli taomi osh
+# s_taomlar = {'otam':'osh', 'onam':'sho\'rva', 'buvam':'somsa', 'bobom':'manti', 'men':'shirguruch'}
+# print(f"Otamning sevimli taomi {s_taomlar['otam']}, Onamning sevimli taomi {s_taomlar['onam']}\
+#  Mening sevimli taomim {s_taomlar['men']}")
+
+#3 Python izohli lu'gati tuzing: Lug'atga shu kunga qadar o'rgangan 10 ta so'z (atamani)
+# kiriting (masalan integer, float, string, if, else va hokazo) va har birining qisqacha
+# tarjimasini yozing.
+# python = {'int':'Butun son','float':'O\'nlik son','str':'Matn','bool':'Mantiqiy',\
+# 'if':'Agar shart operatori','else':'Yoki shart operatori','elif':'Agar va yokini chachishgani',\
+# 'or':'Yoki operatori','and':'Va operatori','in':'Ro\'yxatlarni tekshiruvchi operator'}
+    
+#4 Foydalanuvchidan biror so'z kiritishni so'rang va so'zning tarjimasini yuqoridagi
+# lug'atdan chiqarib bering. Agar so'z lu'gatda mavjud bo'lmasa, "Bunda so'z mavjud emas"
+# degan xabarni chiqaring.
+# python = {'int':'Butun son','float':'O\'nlik son','str':'Matn','bool':'Mantiqiy',\
+# 'if':'Agar shart operatori','else':'Yoki shart operatori','elif':'Agar va yokini chachishgani',\
+# 'or':'Yoki operatori','and':'Va operatori','in':'Ro\'yxatlarni tekshiruvchi operator'}
+
+# soz = input("Biron bir so'z kiriting: ")
+# print(python.get(soz, "Bunday so'z lug'atimizda yo'q"))
+
+#5 Yuqoridagi vazifani if-else yordamida qiling va natijani ham foydalanuvchiga tushunarli
+# ko'rinishda chiqaring.
+python = {'int':'Butun son','float':'O\'nlik son','str':'Matn','bool':'Mantiqiy',\
+'if':'Agar shart operatori','else':'Yoki shart operatori','elif':'Agar va yokini chachishgani',\
+'or':'Yoki operatori','and':'Va operatori','in':'Ro\'yxatlarni tekshiruvchi operator'}
+
+soz = input("Birorta kalit so'z kiriting: ").lower()
+tarjima = python.get(soz)
+if tarjima == None:
+    print("Lug'atimizda bunday so'z yoq.")
+else:
+    print(f"{soz.title()} so'zi {tarjima} deb tarjima qilinadi.")
+    
